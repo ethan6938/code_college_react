3adcc9822bee5485f980fe5ee36171f00ee146cc
ethan6938 <en.hurwitz@gmail.com>
2025-05-23 09:09:57 +0200

    chpter4

--- chapter03/src/App.js
+++ chapter03/src/App.js
@@ -2,20 +2,23 @@ import React, { Component } from 'react';
 import Products from './Products';
 import { Button } from 'react-bootstrap';
 import Rating from './Rating';
+import './App.css';
 
 class App extends Component {
   render() {
     const isValid = true;
     return (
-      <div>
+      <div className="app-container">
         <Products />
         <Button variant="primary" disabled={!isValid}>Default</Button>
 
-        <Rating rating="1" />
-        <Rating rating="2" />
-        <Rating rating="3" />
-        <Rating rating="4" />
-        <Rating rating="5" />
+        <div className="ratings-section">
+          <Rating rating="1" />
+          <Rating rating="2" />
+          <Rating rating="3" />
+          <Rating rating="4" />
+          <Rating rating="5" />
+        </div>
       </div>
     );
   }
