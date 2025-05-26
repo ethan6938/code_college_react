c64e8396cec7ed7abbe971badc04fa4ac7890011
ethan6938 <en.hurwitz@gmail.com>
2025-05-26 08:35:27 +0200

    Update

--- chapter04/src/Rating.js
+++ chapter04/src/Rating.js
@@ -10,36 +10,21 @@ class Rating extends Component {
     handleClick(ratingValue) {
         this.setState({ rating: ratingValue })
     }
-
+    
     render() {
         return (
             <div style={styles.starStyle}>
                 <h1>Rating: {this.state.rating}</h1>
-                {this.state.rating >= 1 ? (
-                    <IoIosStar onClick={this.handleClick.bind(this, 1)} />
-                ) : (
-                    <IoIosStarOutline onClick={this.handleClick.bind(this, 1)}/>
-                )}
-                {this.state.rating >= 2 ? (
-                    <IoIosStar onClick={this.handleClick.bind(this, 2)} />
-                ) : (
-                    <IoIosStarOutline onClick={this.handleClick.bind(this, 2)}/>
-                )}
-                {this.state.rating >= 3 ? (
-                    <IoIosStar onClick={this.handleClick.bind(this, 3)} />
-                ) : (
-                    <IoIosStarOutline onClick={this.handleClick.bind(this, 3)}/>
-                )}
-                {this.state.rating >= 4 ? (
-                    <IoIosStar onClick={this.handleClick.bind(this, 4)} />
-                ) : (
-                    <IoIosStarOutline onClick={this.handleClick.bind(this, 4)}/>
-                )}
-                {this.state.rating >= 5 ? (
-                    <IoIosStar onClick={this.handleClick.bind(this, 5)} />
-                ) : (
-                    <IoIosStarOutline onClick={this.handleClick.bind(this, 5)}/>
-                )}
+                {[1, 2, 3, 4, 5].map((star) => (
+                    <span key={star}>
+                        {this.state.rating >= star && (
+                            <IoIosStar onClick={() => this.handleClick(star)} />
+                        )}
+                        {this.state.rating < star && (
+                            <IoIosStarOutline onClick={() => this.handleClick(star)} />
+                        )}
+                    </span>
+                ))}
             </div>
         );
     }
